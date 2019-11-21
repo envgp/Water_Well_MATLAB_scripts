4027b75191fde681649548c4ba9fd191a3d43b41
Matthew Lees <mlees@stanford.edu>
2019-11-20 20:19:48 -0800

    modifications to work with nicely data

--- functions/remove_measurements_wo_wells.m
+++ functions/remove_measurements_wo_wells.m
@@ -3,9 +3,13 @@ function Data_filt = remove_measurements_wo_wells(Data)
 % have no information about. This function deletes all measurement data which
 % does not have a corresponding well.
 
-    listmeasurement_withwells = ismember(Data.MeasurementData.site_code,Data.WellData.site_code); % Create a list of logicals showing which index in measurements contain a Well .
+    listmeasurement_withwells = ismember(Data.MeasurementData.site_code,Data.WellData.site_code) | ismember(Data.MeasurementData.nicely_site_code,Data.WellData.nicely_site_code); % Create a list of logicals showing which index in measurements contain a Well .
     
-    listwells = true([length(Data.WellData.site_code),1]); % there are no redundant wells in this script...
+     % Manually set wells with no entry for site code/nicely entry to 0.
+    listmeasurement_withwells(Data.MeasurementData.site_code=="") = 0;
+    listmeasurement_withwells(Data.MeasurementData.nicely_site_code=="") = 0;
 
-    Data_filt = filter_logical(Data,listwells,listmeasurement_withwells);
+    %listwells = true([length(Data.WellData.site_code),1]); % there are no redundant wells in this script...
+
+    Data_filt = filter_logical_new(Data,'MeasurementData',listmeasurement_withwells);
 end
