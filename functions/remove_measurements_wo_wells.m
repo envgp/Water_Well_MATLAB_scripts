aeee2e72a1909cda125c5b62a7755a4ebad6e491
Matthew Lees <mlees@stanford.edu>
2019-11-21 11:36:46 -0800

    fixed a bug where entries in well_id of "" caused every well to be deleted. should now work with casgem and nicely data.

--- functions/remove_measurements_wo_wells.m
+++ functions/remove_measurements_wo_wells.m
@@ -2,13 +2,12 @@ function Data_filt = remove_measurements_wo_wells(Data)
 % Sometimes you will have measurements which correspond to wells which you
 % have no information about. This function deletes all measurement data which
 % does not have a corresponding well.
-
-    listmeasurement_withwells = ismember(Data.MeasurementData.site_code,Data.WellData.site_code) | ismember(Data.MeasurementData.nicely_site_code,Data.WellData.nicely_site_code); % Create a list of logicals showing which index in measurements contain a Well .
     
-     % Manually set wells with no entry for site code/nicely entry to 0.
-    listmeasurement_withwells(Data.MeasurementData.site_code=="") = 0;
-    listmeasurement_withwells(Data.MeasurementData.nicely_site_code=="") = 0;
-
+    site_codes = Data.WellData.site_code(Data.WellData.site_code ~= ""); % get a list of site codes excluding empty strings
+    nicely_codes = Data.WellData.nicely_site_code(Data.WellData.nicely_site_code ~= ""); % get a list of nicely codes excluding empty strings
+    
+    listmeasurement_withwells = ismember(Data.MeasurementData.site_code,site_codes) | ismember(Data.MeasurementData.nicely_site_code,nicely_codes); % Create a list of logicals showing which index in measurements contain a Well .
+    
     %listwells = true([length(Data.WellData.site_code),1]); % there are no redundant wells in this script...
 
     Data_filt = filter_logical_new(Data,'MeasurementData',listmeasurement_withwells);
