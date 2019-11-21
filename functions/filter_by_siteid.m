c534c1742d66cd818949bab8cf1cd2bff6b093e9
Matthew Lees <mlees@stanford.edu>
2019-11-21 11:35:20 -0800

    improved help

--- functions/filter_by_siteid.m
+++ functions/filter_by_siteid.m
@@ -1,4 +1,5 @@
 function Data_filt = filter_by_siteid(Data, well_id)
+% Data_filt = filter_by_siteid(Data, well_id)
 % Takes a Data structure and a site_id, and returns a Data structure
 % corresponding to just that site_id. Currently only works for a single
 % well_id.  well_id refers to site_code in OpenData Periodic Groundwater Measurements and the Water Data Library, but is CASGEM
