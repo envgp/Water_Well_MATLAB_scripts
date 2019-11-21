25e2a202b5621e8b4bc87f1446b16caebc7310e0
Matthew Lees <mlees@stanford.edu>
2019-11-21 15:33:21 -0800

    fixed a 'bug' so it now splits based on both wells and measurements (b4 was only working for measurements and some cross-listed wells were appearing twice)

--- functions/split_by_source.m
+++ functions/split_by_source.m
@@ -2,11 +2,16 @@ function [CASGEMdata, Nicelydata] = split_by_source(Data)
 % CASGEMdata, Nicelydata = split_datastructure_by_source(Data)
 %
 % Takes a datastructure and returns two datastructures, corresponding to
-% data from CASGEM and data from Nicely. Filters on measurement, so will
-% LOSE any wells without corresponding measurements.
+% data from CASGEM and data from Nicely. Filters on wells, so will
+% LOSE any measurements without corresponding wells.
 
-logicalcasgem = ismember(Data.MeasurementData.datasource,['CASGEM']);
-logicalnicely = ismember(Data.MeasurementData.datasource,['Nicely']);
+% Split by Wells
+logicalcasgem = ismember(Data.WellData.datasource,['CASGEM']);
+logicalnicely = ismember(Data.WellData.datasource,['Nicely']);
 
-CASGEMdata = filter_logical_new(Data,'MeasurementData',logicalcasgem);
-Nicelydata = filter_logical_new(Data,'MeasurementData',logicalnicely);
+CASGEMdata = filter_logical_new(Data,'WellData',logicalcasgem);
+Nicelydata = filter_logical_new(Data,'WellData',logicalnicely);
+
+% Split by Measurements
+CASGEMdata = filter_logical_new(CASGEMdata,'MeasurementData',ismember(CASGEMdata.MeasurementData.datasource,['CASGEM']));
+Nicelydata = filter_logical_new(Nicelydata,'MeasurementData',ismember(Nicelydata.MeasurementData.datasource,['Nicely']));
