524c3181da353f4762d9fda36425e584c49062e0
Matthew Lees <mlees@stanford.edu>
2019-11-21 15:32:37 -0800

    changes to make it compatible with nicely and casgem.

--- functions/remove_wells_wo_measurements.m
+++ functions/remove_wells_wo_measurements.m
@@ -3,7 +3,11 @@ function Data_filt = remove_wells_wo_measurements(Data)
     
     %fprintf('Running remove_wells_wo_measurements.\n')
     
-    listwells_withmeas = ismember(Data.WellData.stn_id(:),Data.MeasurementData.stn_id(:)) | ismember(Data.WellData.nicely_site_code(:),Data.MeasurementData.nicely_site_code(:));
+    site_codes = Data.MeasurementData.site_code(Data.MeasurementData.site_code ~= ""); % get a list of site codes excluding empty strings
+    nicely_codes = Data.MeasurementData.nicely_site_code(Data.MeasurementData.nicely_site_code ~= ""); % get a list of nicely codes excluding empty strings
+
+    
+    listwells_withmeas = ismember(Data.WellData.site_code,site_codes) | ismember(Data.WellData.nicely_site_code(:),nicely_codes);
     
     Data_filt = Data;
     
