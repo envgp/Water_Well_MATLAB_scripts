1629335a243d3de40882d23a8799e0fcc2409481
Matthew Lees <mlees@stanford.edu>
2019-11-26 17:53:45 -0800

    updated help and made it print out useful information

--- functions/temporal_filter_yearrange.m
+++ functions/temporal_filter_yearrange.m
@@ -1,6 +1,12 @@
 function Data_filt = temporal_filter_yearrange(Data,STARTYYYY,ENDYYYY)
+% Data_filt = temporal_filter_yearrange(Data,STARTYYYY,ENDYYYY)
 % Filters returns Measurements and corresponding wells for a given year.
-
+    
+    fprintf('\nRunning temporal_filter_yearrange\n')
+    fprintf('\tStarting with %s containing %i wells and %i measurements.\n',inputname(1),length(Data.WellData.stn_id(:)),length(Data.MeasurementData.stn_id(:)))
+    
+    fprintf('\tFiltering between years %i and %i.\n', STARTYYYY,ENDYYYY)
+    
     years = year(Data.MeasurementData.date);
     
     measurementslogical = (STARTYYYY <= years & years <= ENDYYYY);
@@ -9,4 +15,7 @@ function Data_filt = temporal_filter_yearrange(Data,STARTYYYY,ENDYYYY)
     
     Data_tmp = filter_logical(Data,wellslogical,measurementslogical);
     Data_filt = remove_wells_wo_measurements(Data_tmp);
+    
+    fprintf('\tFinished with %i wells and %i measurements.\n',length(Data_filt.WellData.stn_id(:)),length(Data_filt.MeasurementData.stn_id(:)))
+
 end
