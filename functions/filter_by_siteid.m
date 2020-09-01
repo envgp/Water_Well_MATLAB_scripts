0814825ba703e5f2aa192804443c6007951bc6aa
Matthew Lees <mlees@stanford.edu>
2020-09-01 11:38:46 -0700

    add history suppor

--- functions/filter_by_siteid.m
+++ functions/filter_by_siteid.m
@@ -5,9 +5,11 @@ function Data_filt = filter_by_siteid(Data, well_id)
 % well_id.  well_id refers to site_code in OpenData Periodic Groundwater Measurements and the Water Data Library, but is CASGEM
 % id in the CASGEM dataset. well_id should be a character vector of form
 % (for example) '361089N1194293W001'.
+    cmd_called = getLastMATLABcommandLineStr();
 
     LIA = ismember(Data.WellData.site_code(:),well_id);
 
     Data_filt = filter_logical_new(Data,'WellData', LIA);
+    Data_filt.History = Data_filt.History + newline + cmd_called;
 
 end
