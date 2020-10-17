a65ae382ec01df0d90a28d38765236124f32ae4b
Matthew Lees <mlees@stanford.edu>
2020-10-17 16:48:55 -0700

    now worked no matter where it is called

--- functions/list_functions.m
+++ functions/list_functions.m
@@ -1,4 +1,16 @@
 function list_functions()
 % lists functions available in the current version of well_data_MATLAB.
 fprintf("LISTING FUNCTIONS IN CURRENT VERSION OF well_data_MATLAB:\n");
-dir ../functions/*.m
+p = path;
+P = split(path,':');
+A = strfind(P,'functions');
+idx_of_functions = find(~cellfun(@isempty,A)); 
+directory = P(idx_of_functions);
+
+a = ls(char(strcat(directory,'/*.m')));
+b = splitlines(a);
+for i = 1:length(b)
+    tmp = split(b(i),'/');
+    disp(tmp{end})
+end
+end
