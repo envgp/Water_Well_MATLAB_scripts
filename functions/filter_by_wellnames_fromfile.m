301ea1c404c12d01b591609ba79cf9edecff441a
Matthew Lees <mlees@stanford.edu>
2021-05-18 18:13:31 -0700

    should now work with site ids, site codes or nicely ids

--- functions/filter_by_wellnames_fromfile.m
+++ functions/filter_by_wellnames_fromfile.m
@@ -10,9 +10,11 @@ wellnames=C{1};
 wellnames_nicely = wellnames(startsWith(wellnames,'K'));
 %wellnames_stnid = wellnames(~isnan(str2double(wellnames))); If i need,
 %this is a slightly better way than the one below.
+cellength = cellfun('length',wellnames);
+wellnames_sitecodes = wellnames(cellength>10);
 wellnames_other = str2double(wellnames(~startsWith(wellnames,'K')));
 
-logical_wells = ismember(Data.WellData.stn_id,wellnames_other) | ismember(Data.WellData.nicely_site_code,wellnames_nicely);
+logical_wells = ismember(Data.WellData.stn_id,wellnames_other) | ismember(Data.WellData.nicely_site_code,wellnames_nicely) | ismember(Data.WellData.site_code,wellnames_sitecodes);
 
 Data_filt = filter_logical_new(Data,'WellData',logical_wells);
 Data_filt.History = Data_filt.History + newline + "Fitered to only contain wellnames from file " + filename +".";
