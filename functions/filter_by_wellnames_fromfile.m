693d3fa88be6ec0cdf05cf14193dc404540e3e12
Matthew Lees <mlees@stanford.edu>
2021-06-23 15:08:19 -0700

    initial upload? or total rewrite anyway

--- functions/filter_by_wellnames_fromfile.m
+++ functions/filter_by_wellnames_fromfile.m
@@ -1,6 +1,13 @@
 function Data_filt = filter_by_wellnames_fromfile(Data,filename)
 % Takes a filename of a text file (in double parenthesis "FILENAME", please), containing either nicely, or stn_ids each line. Well names refer to the 5 digit casgem code, or the full Nicely 'KSB-XXXX' code. 
 
+fprintf('\nRunning filtr_by_wellnames_fromfile\n')
+fprintf('\tStarting with %s containing %i wells and %i measurements.\n',inputname(1),length(Data.WellData.site_code(:)),length(Data.MeasurementData.site_code(:)))
+
+
+fprintf('\tFiltering with the following wellnames file: "%s".\n',filename)
+
+
 fileID = fopen(filename);
 C = textscan(fileID,'%s','headerlines',1);
 fclose(fileID);
@@ -18,4 +25,6 @@ logical_wells = ismember(Data.WellData.stn_id,wellnames_other) | ismember(Data.W
 
 Data_filt = filter_logical_new(Data,'WellData',logical_wells);
 Data_filt.History = Data_filt.History + newline + "Fitered to only contain wellnames from file " + filename +".";
+
+fprintf('\tFinished with %i wells and %i measurements.\n',length(Data_filt.WellData.site_code(:)),length(Data_filt.MeasurementData.site_code(:)))
 end
